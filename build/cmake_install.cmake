db245079e164526774ab1ccc235cb58ed2b394a9
Maxim <maxman_07@bk.ru>
2020-09-21 00:23:35 +0500

    Added glad

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -35,6 +35,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("C:/Users/Maxim/Documents/VR/myProj/build/external/glfw/cmake_install.cmake")
+  include("C:/Users/Maxim/Documents/VR/myProj/build/external/glad/cmake_install.cmake")
 
 endif()
 
